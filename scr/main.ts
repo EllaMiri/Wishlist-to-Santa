0556950730cd9de198210abffe82e40f65e6f529
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2021-12-15 15:17:56 +0100

    added basic layout

--- scr/main.ts
+++ scr/main.ts
@@ -7,8 +7,20 @@ interface WishList {
 
 const santasWishlists: WishList[] = [
     {
-    list: [],
+    list: ['Tesla', 'PS5'],
+    name: 'Nathaniel',
+    isNaughty: true
+    },
+
+    {
+    list: ['Presentkort'],
     name: 'Ella',
+    isNaughty: false
+    },
+
+    {
+    list: ['Ponny'],
+    name: 'Alva',
     isNaughty: false
     }
 ]
