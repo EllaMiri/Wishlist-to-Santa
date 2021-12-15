53a80b9b2701b063aad39c79d8ecffd8f0079565
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2021-12-15 15:02:34 +0100

    added wishlists

--- scr/main.ts
+++ scr/main.ts
@@ -1,1 +1,14 @@
-console.log('hello world!')
+interface WishList {
+    name: string;
+    list: string[];
+    isNaughty: boolean;
+}
+
+
+const santasWishlists: WishList[] = [
+    {
+    list: [],
+    name: 'Ella',
+    isNaughty: false
+    }
+]
