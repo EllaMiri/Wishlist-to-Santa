69c318d5e2d4d1d5d6c797ecfce4e9863e10d9c2
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2021-12-15 16:58:51 +0100

    added renders

--- scr/main.ts
+++ scr/main.ts
@@ -4,7 +4,6 @@ interface WishList {
     isNaughty: boolean;
 }
 
-
 const santasWishlists: WishList[] = [
     {
     list: ['Tesla', 'PS5'],
@@ -24,3 +23,40 @@ const santasWishlists: WishList[] = [
     isNaughty: false
     }
 ]
+
+window.addEventListener('load', main);
+
+function main() {
+    renderSantasWishlists();
+}
+
+function renderSantasWishlists() {
+    const mainElement = document.querySelector('main')
+    if (!mainElement) return;
+
+    mainElement.innerHTML = '';
+
+    for (const wishlist of santasWishlists) {
+      const div = createSingleWishlistElement(wishlist);
+        mainElement.append(div)
+    }
+}
+
+function createSingleWishlistElement(wishlist: WishList) {
+    const div = document.createElement('div')
+    div.className = "wish-list-card"
+    
+    const h3 = document.createElement('h3')
+    h3.innerHTML = wishlist.name;
+    div.append(h3)
+    
+    const ul = document.createElement('ul')
+    for (const wish of wishlist.list) {
+        const li = document.createElement('li')
+        li.innerText = wish;
+        ul.append(li)
+    }
+    div.append(ul)
+    return div;
+
+}
